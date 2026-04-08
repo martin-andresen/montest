c2308b72c0db40a2cfdb4dda733ecf7971450ae8
Martin Eckhoff Andresen <martin.eckhoff.andresen@gmail.com>
2026-04-08 14:35:32 +0200

    Update fct_datasim.R
    harmonized DGPs to have same average size and magnitude of violations.

--- R/fct_datasim.R
+++ R/fct_datasim.R
@@ -9,6 +9,119 @@ fct_datasim <- function(setup, dgp, n) {
   p <- 3
   betaXY <- c(0.3, 0.3, 0.3)
   cov <- matrix(c(1, 0.3, 0.3, 1), 2, 2)
+  errors <- MASS::mvrnorm(n, rep(0, 2), cov)
+
+  X <- matrix(rnorm(n * p), n, p)
+  colnames(X) <- paste0("Xvar", 1:p)
+
+  # common alpha levels for all monotonicity DGPs
+  alpha_lo <- -0.75
+  alpha_hi <-  0.40
+
+  if (dgp == 0) {
+    # no violations of A1 & A3; inequalities are binding
+    alpha <- rep(0, n)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 1) {
+    # no violations of A1 & A3; inequalities are not binding
+    alpha <- rep(0.2, n)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 2) {
+    # local violation of monotonicity: axis-aligned threshold
+    alpha <- ifelse(X[, 1] < qnorm(0.15), alpha_lo, alpha_hi)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 3) {
+    # local violation of exclusion restriction
+    alpha <- rep(0.2, n)
+    gamma <- ifelse(X[, 2] < qnorm(0.15), 1.25, 0)
+
+  } else if (dgp == 4) {
+    # global violation of exclusion restriction
+    alpha <- rep(0.2, n)
+    gamma <- rep(0.5, n)
+
+  } else if (dgp == 5) {
+    # global violation of exclusion restriction with sign heterogeneity
+    alpha <- rep(0.2, n)
+    gamma <- ifelse(X[, 3] < qnorm(0.5), 0.5, -0.5)
+
+  } else if (dgp == 6) {
+    # radial local violation of monotonicity
+    # violating region is the inner 15% ball under ||X||^2 ~ chi^2_3
+    r2 <- rowSums(X^2)
+    cutoff6 <- qchisq(0.15, df = 3)
+    alpha <- ifelse(r2 < cutoff6, alpha_lo, alpha_hi)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 7) {
+    # oblique local violation of monotonicity
+    beta_rot <- c(1, 1, 1) / sqrt(3)
+    S <- as.vector(X %*% beta_rot)
+    alpha <- ifelse(S < qnorm(0.15), alpha_lo, alpha_hi)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 8) {
+    # oscillating local violation of monotonicity
+    # define violating region as top 15% of oscillation score
+    U1 <- pnorm(X[, 1])
+    U2 <- pnorm(X[, 2])
+    V  <- sin(pi * U1) * sin(pi * U2)
+
+    # approx 85th percentile of V when U1,U2 ~ U(0,1)
+    cutoff8 <- 0.7777
+    alpha <- ifelse(V > cutoff8, alpha_lo, alpha_hi)
+    gamma <- rep(0, n)
+
+  } else if (dgp == 9) {
+    # smooth-index local violation of monotonicity
+    beta_idx <- c(1, 1, 1) / sqrt(3)
+    S <- as.vector(X %*% beta_idx)
+    alpha <- ifelse(S > qnorm(0.85), alpha_lo, alpha_hi)
+    gamma <- rep(0, n)
+  } else if (dgp == 10) {
+    # oblique local violation of exclusion restriction
+    a <- 0.2
+    alpha <- rep(a, n)
+
+    beta_exc <- c(1, 1, 1) / sqrt(3)
+    S <- as.vector(X %*% beta_exc)
+    gamma <- ifelse(S > qnorm(0.85), 1.25, 0)
+  } else {
+    stop("invalid choice of dgp")
+  }
+
+  if (setup == "A") {
+    # randomized instrument
+    Z <- rbinom(n, size = 1, prob = 0.5)
+    Dlatent <- alpha * Z + errors[, 1]
+    D <- as.numeric(Dlatent > 0)
+
+  } else if (setup == "B") {
+    # unconfounded instrument
+    Zlatent <- 0.2 * X[, 1] + 0.2 * X[, 2] + 0.2 * X[, 3] + rnorm(n)
+    Z <- as.numeric(Zlatent > 0)
+    b <- 0.5 * log(1 + exp(X[, 1] + X[, 2] + X[, 3]))
+    Dlatent <- b + alpha * Z + errors[, 1]
+    D <- as.numeric(Dlatent > 0)
+
+  } else {
+    stop("invalid choice of setup")
+  }
+
+  Y <- as.vector(1 * D + gamma * Z + X %*% betaXY + errors[, 2])
+
+  data <- as.data.frame(cbind(Y, D, Z, X))
+  return(data)
+}
+
+fct_datasim_old <- function(setup, dgp, n) {
+
+  p <- 3
+  betaXY <- c(0.3, 0.3, 0.3)
+  cov <- matrix(c(1, 0.3, 0.3, 1), 2, 2)
   errors <- mvrnorm(n, rep(0, 2), cov)
 
   X <- matrix(rnorm(n * p), n, p)
@@ -119,51 +232,4 @@ fct_datasim <- function(setup, dgp, n) {
   return(data)
 }
 
-fct_datasim_old <- function(setup,dgp,n){
-  p <- 3
-  betaXY <- c(0.3,0.3,0.3)
-  cov <- matrix(c(1,0.3,0.3,1),2,2)
-  errors <-(mvrnorm(n,rep(0,2),cov))
-  X <- matrix(rnorm(n*p), n, p)
-  colnames(X) <- paste("Xvar", 1:p, sep="")
-
-  if(dgp==0) {          # no violations of A1 & A3; inequalities are binding
-    a <- 0; alpha =  rep(a, n)
-    g <- 0; gamma = rep(g, n)
-  } else if(dgp==1) {   # no violations of A1 & A3; inequaltities are not binding (all tests are conservative)
-    a <- 0.2; alpha =  rep(a, n)
-    g <- 0;   gamma = rep(g, n)
-  } else if(dgp==2) {   # local violation of monotonicity
-    alpha <- as.numeric(ifelse(X[,1]< qnorm(0.15), -0.75, 0.4))
-    g <- 0; gamma = rep(g, n)
-  } else if(dgp==3) {   # local violation of exclusion restriction
-    a <- 0.2; alpha =  rep(a, n)
-    gamma <- as.numeric(ifelse(X[,2]< qnorm(0.15), 1.25, 0))
-  } else if(dgp==4) {   # global violation of exclusion restriction
-    a <- 0.2; alpha =  rep(a, n)
-    g <- 0.5; gamma = rep(g, n)
-  } else if(dgp==5) {   # global violation of exclusion restriction but with sign heterogeneity
-    a <- 0.2; alpha =  rep(a, n)
-    gamma <- as.numeric(ifelse(X[,3]< qnorm(0.5), 0.5, -0.5))
-  } else {
-    stop("invalid choice of dgp")
-  }
-
-  if (setup == 'A'){         # basic setup (randomized experiment)
-    Z <- rbinom(n, size=1, prob=0.5)
-    Dlatent <- alpha*Z+errors[,1]
-    D<-as.numeric( Dlatent>0 )
-  } else if (setup == 'B'){  # easy propensity and strong confounding
-    Zlatent = 0.2*X[,1] + 0.2*X[,2] + 0.2*X[,3] + rnorm(n)
-    Z = as.numeric( Zlatent>0 )
-    b = 0.5 * log(1 + exp(X[,1] + X[,2] + X[,3]))
-    Dlatent = b + alpha*Z + errors[,1]
-    D = as.numeric( Dlatent > 0 )
-  } else {
-    stop("invalid choice of setup")
-  }
-  Y <- as.vector(1*D+gamma*Z+X%*%betaXY+errors[,2])
-  data <- as.data.frame(cbind(Y,D,Z,X))   # if we add further variables to data, then the call of LATEtest.R in the simulation file has to be adapted!
-  return(data)
-}
 
